e87e75130a3b3c31e1261367da8d89cab5a0ca08
raisoumayma97 <raisoumayma97@yahoo.com>
2025-02-08 13:46:01 +0100

    Update MovieDetails.jsx

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -3,12 +3,8 @@ import { useParams } from "react-router-dom";
 import movieItem from "../movie";
 
 const MovieDetails = () => {
-  const { title } = useParams(); // Get title from URL
-  const movie = movieItem.find((movie) => movie.title === title); // Find the movie by title
-
-  if (!movie) {
-    return <div>Movie not found</div>; // Handle the case where the movie isn't found
-  }
+  const { title } = useParams(); 
+  const movie = movieItem.find((movie) => movie.title === title); 
 
   return (
     <div className="warning-bg p-4 rounded-lg">
